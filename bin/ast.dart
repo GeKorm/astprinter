1666721476abf3807af0089347d9844f06880718
GeKorm <GeKorm@users.noreply.github.com>
2015-04-24 01:40:36 +0300

    Added 2 optional parameters
    -dec prints all (nested) declarations, -dir prints all directives

--- bin/ast.dart
+++ bin/ast.dart
@@ -1,59 +1,103 @@
 import 'dart:io';
-
+import 'dart:mirrors';
 import 'package:analyzer/analyzer.dart';
 import 'package:analyzer/src/generated/ast.dart';
 
-main(List<String> args) {
-  if (args.length != 1) {
-    print("Usage: ast <path>");
-    exit(1);
-  }
-  
-  CompilationUnit unit = parseDartFile(args[0]);
-  print(expandedAst(unit));
+bool isType(dynamic obj, ClassMirror sec) {
+  return reflect(obj).type.isSubtypeOf(sec);
 }
 
-String expandedAst(CompilationUnit u) {
+main(List<String> args) {
+  if (args.length == 1) {
+    CompilationUnit unit = parseDartFile(args[0]);
+    ClassMirror mirror = reflectClass(Object);
+    stdout.write(expandedAst(unit, mirror));
+  } else if (args.length == 2) {
+    if (args[1] == '-dir') {
+      CompilationUnit unit = parseDartFile(args[0]);
+      stdout.write(astDirectives(unit));
+    } else if (args[1] == '-dec') {
+      CompilationUnit unit = parseDartFile(args[0]);
+      ClassMirror mirror = reflectClass(Declaration);
+      stdout.write(expandedAst(unit, mirror));
+    } else {
+      printUsageError();
+    }
+  } else {
+    printUsageError();
+  }
+}
+
+String expandedAst(CompilationUnit u, ClassMirror c) {
   Iterable children = u.childEntities;
-  getRecursiveChildren(children);
-  String temp = stuff.join();
+  getRecursiveChildren(children, c);
+  String temp = stuff.join('\r');
   stuff.clear();
   return temp;
 }
 
+String astDirectives(CompilationUnit u) {
+  return buildListString(u.directives);
+}
+
 List<String> stuff = [];
 
-void getRecursiveChildren(dynamic cu, [List<String> builder, int count]) {
+String buildListString(NodeList nl) {
+  List<String> builder = [];
+  for (var item in nl) {
+    builder.add(item.toString());
+  }
+  String temp = builder.join();
+  return temp;
+}
+
+void getRecursiveChildren(dynamic cu, ClassMirror nodeType,
+    [List<String> builder, int count]) {
   if (builder == null) {
     builder = [];
     count = 0;
   }
   for (var ch in cu) {
     var temp = null;
-    count++;
-    stuff.add(
-        spaces(count) + ch.runtimeType.toString() + '::: ' + ch.toString() + '\n');
+    if (isType(ch, nodeType)) {
+      count++;
+      stuff.add(
+          spaces(count) + ch.runtimeType.toString() + '::: ' + ch.toString());
+    }
     try {
       temp = ch.childEntities;
     } catch (exception, stackTrace) {
       //
     }
+
     if (temp != null) {
-      builder.add(stuff.toString() + spaces(count) + '\n\n');
-      getRecursiveChildren(temp, builder, count);
-      count--;
+      if (isType(ch, nodeType)) {
+        builder.add(stuff.toString());
+        getRecursiveChildren(temp, nodeType, builder, count);
+        count--;
+      } else {
+        builder.add(stuff.toString());
+        getRecursiveChildren(temp, nodeType, builder, count);
+      }
     }
   }
 }
 
+void printUsageError() {
+  print('''Usage: ast <path> <-optional>
+  -dir prints directives"
+  -dec prints declarations''');
+  exit(1);
+}
+
 String spaces(int c) {
   List<String> spac = [];
-  if (c == 0) {
-    return '';
-  } else if (c > 0) {
+  if (c > 0) {
     for (int i = 0; i < c; i++) {
       spac.add('  ');
     }
     return spac.join();
+  } else {
+    return '';
   }
 }
